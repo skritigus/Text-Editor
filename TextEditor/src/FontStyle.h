60ca6d88768e20b85855284d4c2a83736fa3fb82
skritigus <sdynkovich@gmail.com>
2024-11-12 22:16:02 +0300

    Fix sonar #3

--- TextEditor/src/FontStyle.h
+++ TextEditor/src/FontStyle.h
@@ -15,7 +15,11 @@ public:
     FontStyle() = default;
     FontStyle (QFont font, QColor textColor, QColor backgroundColor, Qt::Alignment align) :
         font(font), textColor(textColor), backgroundColor(backgroundColor), align(align) {}
-    FontStyle(const FontStyle& style) = default;
+    FontStyle(const FontStyle& style) :
+        font(style.font), textColor(style.textColor), backgroundColor(style.backgroundColor), align(style.align) {};
+    FontStyle (FontStyle&& other) noexcept :
+        font(other.font), textColor(other.textColor), backgroundColor(other.backgroundColor), align(other.align) {};
+    ~FontStyle() {};
 
     QFont getFont() const;
     QColor getTextColor() const;
@@ -26,6 +30,19 @@ public:
     void setTextColor(QColor newTextColor);
     void setBackgroundColor(QColor newBackgroundColor);
     void setAlign(Qt::Alignment newAlign);
+
+    FontStyle& operator=(FontStyle&& other) noexcept
+    {
+        if (this != &other)
+        {
+            font = other.font;
+            textColor = other.textColor;
+            backgroundColor = other.backgroundColor;
+            align = other.align;
+        }
+
+        return *this;
+    }
 
     FontStyle& operator=(const FontStyle& other) = default;
 };
